c9895b1d66138a6b94796c7879df211380291f01
Tetiana Vrba <tetiana.vrba@gmail.com>
2023-10-02 09:50:15 +0200

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+const goodbye = (name) => {
+  return 'S pozdravem  ' + name;
+};
 
+const formalGoodbye = (name) => {
+  return 'S uctivou poklonou ' + name;
+};
+
+const rudeGoodbye = (name) => {
+  return 'Se měj ' + name;
+};
+
+const fillSubject = (subject) => {
+  document.querySelector('.email__subject').textContent = subject;
+};
+
+const fillBody = (body, name, goodbyeFunction) => {
+  const bodyElm = document.querySelector('.email__body');
+  bodyElm.innerHTML += body;
+  const closingElm = document.querySelector('.email__closing');
+  closingElm.textContent = goodbyeFunction(name);
+};
+
+fillSubject('Nepřišly gumičky');
+fillBody(
+  'Nepřišly gumičky. A co na to počítač? Mlčí. No tak to je konec. Všecko vylejt.',
+  'referent Kubrt',
+  rudeGoodbye,
+);
